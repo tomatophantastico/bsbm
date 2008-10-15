7d81e4a29b34352cf33359dd500d6c5ad0b433b6
andreas_schultz <andreas_schultz@8987fa8a-6640-486f-b76a-4fa311f6c324>
2008-10-15 19:27:09 +0000

    refactored

--- trunk/src/benchmark/validation/ValidatorDefaultValues.java
+++
@@ -1,6 +0,0 @@
-package benchmark.validation;
-
-public class ValidatorDefaultValues {
-	public static final boolean resultsCountOnly=false;
-	public static final String qualificationLog = "qual.log";
-}
