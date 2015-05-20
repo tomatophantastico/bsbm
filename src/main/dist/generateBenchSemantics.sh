96b9acffb8d2dfea084e9e48bc248edb27e22bb9
Jörg Unbehauen <junbehauen@brox.de>
2015-05-20 19:29:14 +0200

    added benchmark setup

---
+++ src/main/dist/generateBenchSemantics.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+for PC in 2848 28480 284800
+do
+	for SER in nqr nqp trig ttl
+	do
+		DIR="./data/10gc/$PC/$SER/"
+		mkdir -p $DIR
+		./generate -pc $PC -s $SER 
+		if [ $SER = "trig" ]; then
+			./generateAuth -fn dataset.nq -uc 50 -gc 10
+			gzip dataset.trig
+		elif [ $SER = "nqr" ] || [ $SER = "nqp" ]; then
+			./generateAuth -fn dataset.nq -uc 50 -gc 10
+			gzip dataset.nq
+		else
+			gzip dataset.ttl
+		fi	
+		
+		mv dataset* $DIR 
+		mv  *.list  $DIR 
+		mv auth* $DIR 
+		mv td_data/ $DIR 
+	done
+done
